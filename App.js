ba4ebe3ad154b608b1fa74abf6fedb4c40c8200e
volecilevo <levent.budakoglu@gmail.com>
2019-10-17 17:11:07 +0300

    dashboard eklemeleri

--- App.js
+++ App.js
@@ -17,10 +17,13 @@ import dataS from './CustomerList';
 import  { Avatar } from 'react-native-elements';
 
 import imageLogo from './assets/bekologo.png';
+import visitIcon from './assets/visit.png';
+import opportunityIcon from './assets/opportunity.png';
+import incomeIcon from './assets/income.png';
 
-import {
-  Container, Header, Left, Body, Right, Icon, Title , Form, Item, Input, Label ,Button as Button2, Text as Text2
-} from 'native-base'
+import {  Card, CardItem, Thumbnail,Content,
+  Container, Header, Left, Body, Right, Icon, Title , Form, Item, Input, Label ,Button as Button2, Text as Text2, Footer, Separator
+} from 'native-base';
 
 class LogoTitle extends React.Component {
   render() {
@@ -119,7 +122,7 @@ class DetailsScreen extends React.Component {
 class LoginScreen extends React.Component{
 
   handleLoginPress = () => {
-    console.log("Login button pressed"); this.props.navigation.navigate('Home');
+    console.log("Login button pressed"); this.props.navigation.navigate('Dashboard');
   };
   static navigationOptions = {
     header: null
@@ -147,10 +150,191 @@ class LoginScreen extends React.Component{
 }
 
 
+class DashboardScreen extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      type: 'column2d',
+      width: '100%',
+      height: '100%',
+      dataFormat: 'json',
+      dataSource: {
+            "chart": {
+                "caption": "Harry's SuperMart",
+                "subCaption": "Top 5 stores in last month by revenue",
+                "numberprefix": "$",
+                "theme": "fint"
+            },
+            "data": [
+                {
+                    "label": "Bakersfield Central",
+                    "value": "880000"
+                },
+                {
+                    "label": "Garden Groove harbour",
+                    "value": "730000"
+                }, 
+                {
+                    "label": "Los Angeles Topanga",
+                    "value": "590000"
+                },
+                {
+                    "label": "Compton-Rancho Dom",
+                    "value": "520000"
+                },
+                {
+                    "label": "Daly City Serramonte",
+                    "value": "330000"
+                }
+            ]
+        }
+    };
+
+    this.libraryPath = './assets/fusioncharts.html';
+  } 
+  render(){
+    return(
+      <Container>
+      <Header />
+      <Content style={styles.cardStyle}>
+        <Card>
+          <CardItem>
+            <Body style={styles.levo}>
+              <Thumbnail style={styles.levo} source={visitIcon} />
+              <Text2 style={styles.levo}>Total Visits</Text2>
+              <Text2 style={styles.levo}>15</Text2>
+            </Body>
+          </CardItem>
+          <CardItem>
+            <Body
+  style={{
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  }}
+>
+            </Body>
+          </CardItem>
+          <CardItem style={styles.cardItemPadding}>
+            <Left>
+                <Text>Completed Plans</Text>
+            </Left>
+            <Right>
+              <Text>6</Text>
+            </Right>
+          </CardItem>
+          <CardItem style={styles.cardItemPadding}>
+            <Left>
+                <Text>Incompleted Plans</Text>
+            </Left>
+            <Right>
+              <Text>9</Text>
+            </Right>
+          </CardItem>
+          <CardItem style={styles.cardItemPaddingLast}>
+            <Left>
+                <Text>Average Visits Per Day</Text>
+            </Left>
+            <Right>
+              <Text>2</Text>
+            </Right>
+          </CardItem>
+        </Card>
+        <Card>
+          <CardItem>
+            <Body style={styles.levo}>
+              <Thumbnail style={styles.levo} source={opportunityIcon} />
+              <Text2 style={styles.levo}>Total Opportunity</Text2>
+              <Text2 style={styles.levo}>$ 1.500.000</Text2>
+            </Body>
+          </CardItem>
+          <CardItem>
+            <Body
+  style={{
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  }}
+>
+            </Body>
+          </CardItem>
+          <CardItem style={styles.cardItemPadding}>
+            <Left>
+                <Text>Total Number of Clients</Text>
+            </Left>
+            <Right>
+              <Text>85</Text>
+            </Right>
+          </CardItem>
+          <CardItem style={styles.cardItemPadding}>
+            <Left>
+                <Text>Active Clients</Text>
+            </Left>
+            <Right>
+              <Text>$ 500.000</Text>
+            </Right>
+          </CardItem>
+          <CardItem style={styles.cardItemPaddingLast}>
+            <Left>
+                <Text>Potential Clients</Text>
+            </Left>
+            <Right>
+              <Text>$ 1.000.000</Text>
+            </Right>
+          </CardItem>
+        </Card>
+        <Card>
+          <CardItem>
+            <Body style={styles.levo}>
+              <Thumbnail style={styles.levo} source={incomeIcon} />
+              <Text2 style={styles.levo}>Total Net Sales</Text2>
+              <Text2 style={styles.levo}>$ 800.000</Text2>
+            </Body>
+          </CardItem>
+          <CardItem>
+            <Body
+  style={{
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  }}
+>
+            </Body>
+          </CardItem>
+          <CardItem style={styles.cardItemPadding}>
+            <Left>
+                <Text>Total Number of Orders</Text>
+            </Left>
+            <Right>
+              <Text>200</Text>
+            </Right>
+          </CardItem>
+          <CardItem style={styles.cardItemPadding}>
+            <Left>
+                <Text>Price Per Order</Text>
+            </Left>
+            <Right>
+              <Text>$ 400</Text>
+            </Right>
+          </CardItem>
+          <CardItem style={styles.cardItemPaddingLast}>
+            <Left>
+                <Text>Number of Unique Products</Text>
+            </Left>
+            <Right>
+              <Text>12</Text>
+            </Right>
+          </CardItem>
+        </Card>
+      </Content>
+    </Container>
+    );
+  }
+}
+
 const RootStack = createStackNavigator(
   {
     Login : LoginScreen,
     Home: HomeScreen,
+    Dashboard: DashboardScreen,
     Details: DetailsScreen,
   },
   {
@@ -236,6 +420,32 @@ const styles = StyleSheet.create({
   userName:{
     fontSize:16,
     color:"#151515",
+  },
+  cardBodyText:{
+    marginLeft:-12,
+  },
+  levo:{
+    alignSelf:"center"
+  },
+  cardStyle:{
+    padding:10
+  },
+  cardItemPadding:{
+    paddingBottom:2,
+    paddingTop:2,
+    fontSize:7
+  },
+  cardItemPaddingLast:{
+    paddingBottom:20,
+    paddingTop:2
+  },
+  heading: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginBottom: 10
+  },
+  chartContainer: {
+    height: 200
   },
   containerLogin: {
     flex: 1,
